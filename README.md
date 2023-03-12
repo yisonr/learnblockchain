c6d31b858425002c69458199be0eb4c883b8de6c
lujetliu <462546997@qq.com>
2023-03-12 15:00:34 +0800

    w1_1 completed

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### 登链社区区块链技术集训营第二期作业
+
+#### 第一周
+##### 第一节课
+- 使用 Metamask	执行一次[转账](https://goerli.etherscan.io/tx/0xb2f07a31ae2ed3311c5c61480f684113595357abe3b8b399d522b9e6029c89d2)
+	![](./w1/w1_1/transaction.png)
+
+- 使用 Remix 创建一个 Counter 合约并部署
+	![](./w1/w1_1/deploy.png)
---
+++ w1/w1_1/Counter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+
+contract Counter {
+	uint public counter;
+	
+	constructor() {
+		counter =0;
+	}
+
+	function count() public {
+		counter = counter +  x
+	}
+
+	function add(x uint) public {
+		counter = counter +  x
+	}
+}
